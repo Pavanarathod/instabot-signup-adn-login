4a9f346fcc8211751b21b8e4e351e2045d6713ff
Pavan Rathod <edroculllen13@gmail.com>
2021-09-27 10:53:59 +0530

    Updating

--- main.py
+++ main.py
@@ -12,28 +12,28 @@ class InstaBot:
 
         self.driver.find_element_by_name("username").send_keys(username)
         self.driver.find_element_by_name('password').send_keys(username)
-        # self.driver.find_element_by_xpath().click()
+        self.driver.find_element_by_xpath().click()
 
 
-InstaBot("dasrockatansky139@gmail.com", "PavanDas@1996")
+InstaBot("Your email", "Your password")
 
 
-# self.driver.implicitly_wait(5)
+self.driver.implicitly_wait(5)
 
-# cookie = self.driver.find_element_by_id('bigCookie')
-# cookie_count = self.driver.find_element_by_id('cookies')
+cookie = self.driver.find_element_by_id('bigCookie')
+cookie_count = self.driver.find_element_by_id('cookies')
 
-# items = [self.driver.find_element_by_id("productPrice" + str(i)) for i in range(1, -1, -1) ]
+items = [self.driver.find_element_by_id("productPrice" + str(i)) for i in range(1, -1, -1) ]
 
 
-# actions = ActionChains(self.driver)
-# actions.click(cookie)
+actions = ActionChains(self.driver)
+actions.click(cookie)
 
-# for i in range(100):
-# 	actions.perform()
-# 	count = int(cookie_count.text.split(" ")[0])
-# 	for item in items:
-# 		value = int(item.text)
-# 		if value <= count:
-# 			upgrade_actions = ActionChains(self.driver)
-# 			update_actions.move_to_element(item)
+for i in range(100):
+	actions.perform()
+	count = int(cookie_count.text.split(" ")[0])
+	for item in items:
+		value = int(item.text)
+		if value <= count:
+			upgrade_actions = ActionChains(self.driver)
+			update_actions.move_to_element(item)
